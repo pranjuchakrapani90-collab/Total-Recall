0adb3a647d77e540e74defc2f32867711967ad43
pranjuchakrapani90-collab <pranjuchakrapani90@gmail.com>
2026-09-01 20:54:31 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE='name-recall-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+  if(e.request.method==='GET' && new URL(e.request.url).origin===location.origin){const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}
+  return resp;
+}).catch(()=>caches.match('./index.html')))));
